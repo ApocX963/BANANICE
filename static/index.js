6fff1993c2f5fc1cceccc4446b328892d7cb4fa1
ApocX963 <98698832+ApocX963@users.noreply.github.com>
2022-01-30 22:40:08 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -441,11 +441,11 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
             SubmitResults();
         }
 
-        if (cps <= 5) return '试着好好练一下？';
+        if (cps <= 5) return '又要绝望再演了？';
         if (cps <= 8) return 'TCL';
         if (cps <= 10)  return 'TQL';
         if (cps <= 15) return '您';
-        return '人？';
+        return '摘星之罪';
     }
 
     function toStr(obj) {
